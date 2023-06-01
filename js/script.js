8f015fc11301ffc4ce52ff85bd70759bfcfdb87b
Amir01020 <amirrodjabov@gmail.com>
2023-06-01 20:50:58 +0500

    next10

--- js/script.js
+++ js/script.js
@@ -2,6 +2,7 @@ let doc = document
 let heads = doc.querySelector('#heads')
 let caunt = doc.querySelector('#caunt')
 let oupenScroll = doc.querySelector('.oupenScroll')
+
 doc.body.onscroll = (event) => {
     console.log();
     if (window.scrollY >= caunt.offsetHeight * 3) {
@@ -21,7 +22,7 @@ doc.body.onscroll = (event) => {
 
 }
 let masc = document.querySelectorAll('#selector');
-for (let i of masc){
+for (let i of masc) {
     let maskOptions = {
         mask: '+{7} (000) 000-00-00',
         lazy: false,
@@ -32,10 +33,10 @@ for (let i of masc){
 //AIzaSyC8vwy2tLXxwXz9HVOtVylxeJvw4YWHBbs
 let mtbox = doc.querySelector('.mtbox')
 
-,filt = doc.querySelectorAll('#filt')
+    , filt = doc.querySelectorAll('#filt')
 
-filt.forEach((i)=>{
-    i.onclick=()=>{
+filt.forEach((i) => {
+    i.onclick = () => {
         filt.forEach(element => {
             element.classList.remove('active')
         });
@@ -44,15 +45,47 @@ filt.forEach((i)=>{
             mtbox.classList.add('filt1')
             mtbox.classList.remove('filt2')
             mtbox.classList.remove('filt3')
-        }else if (i.dataset.name == 'табак') {
+        } else if (i.dataset.name == 'табак') {
             mtbox.classList.add('filt2')
             mtbox.classList.remove('filt1')
             mtbox.classList.remove('filt3')
-        }else{
+        } else {
             mtbox.classList.add('filt3')
             mtbox.classList.remove('filt2')
             mtbox.classList.remove('filt1')
         }
     }
-    
+
 })
+
+let burger = doc.querySelector('.burger')
+let burgerMenu = doc.querySelector('.burgerMenu')
+let menuAct = false
+burger.onclick=()=>{
+    if (menuAct == false) {
+        menuAct = true
+        burgerMenu.classList.add('activeMenu')
+        burger.src ='./icon/Frame 82 (1).png'
+        doc.body.style.overflow = 'hidden'
+    }else{
+        menuAct = false
+        burgerMenu.classList.remove('activeMenu')
+        burger.src ='./icon/Frame 82.png'
+        doc.body.style.overflow = 'auto'
+    }
+    
+}
+let bowl = doc.querySelectorAll('#bowl')
+let tobacco = doc.querySelectorAll('#tobacco')
+function filts(btn) {
+    btn.forEach((i)=>{
+        i.onclick=()=>{
+            for(let item of btn){
+                item.classList.remove('grActive')
+            }
+            i.classList.add('grActive')
+        }
+    })
+}
+filts(bowl)
+filts(tobacco)
